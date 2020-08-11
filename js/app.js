885b3486c9be940e96d7b79cc80a657f54b95c88
TAYLOR JOHNSON <TAYLOR.MCMINN@GMAIL.COM>
2020-08-11 13:25:09 -0700

    added function for h2 names

--- js/app.js
+++ js/app.js
@@ -17,12 +17,18 @@ for (var i=0; i<14; i++){
 
 // object named after city listing each property
 var seattle = {
+  name: 'Seattle',
   minCustRate:23,
   maxCustRate:65,
   avgCookieSale:6.3,
   avgCookieArr:[],
   cookieTotal:0,
+  cityName: function(){
+    var grabName = document.getElementById('store-one-name');
+    grabName.textContent = `${this.name}`;
+  },
   //function to find random customer rate within our min max range
+  //I got this from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   randomMath: function(){
     return Math.ceil(Math.random() * (this.maxCustRate - this.minCustRate + 1)) + this.minCustRate;
   },
@@ -34,7 +40,7 @@ var seattle = {
       this.avgCookieArr.push(Math.ceil(avg));
       total += avg;
     }
-    console.log(total);
+    // console.log(total);
     this.cookieTotal= Math.ceil(total);
   },
   // this creatures an arry of the average cookies per hour for the city.
@@ -55,6 +61,7 @@ var seattle = {
     list.appendChild(lastItem);
   }
 };
+seattle.cityName();
 seattle.generateSales();
 // console.log(seattle.avgCookieArr);
 seattle.render();
@@ -62,6 +69,7 @@ seattle.render();
 
 // this is the object for the city tokyo
 var tokyo = {
+  name: 'Tokyo',
   minCustRate:3,
   maxCustRate:24,
   avgCookieSale:1.2,
@@ -82,6 +90,11 @@ var tokyo = {
     console.log(total);
     this.cookieTotal= Math.ceil(total);
   },
+  //function to add the name property to the h2 element
+  cityName: function(){
+    var grabName = document.getElementById('store-two-name');
+    grabName.textContent = `${this.name}`;
+  },
   // this creatures an arry of the average cookies per hour for the city.
   //this function displays the values of the array as an unordered list.
   render: function(){
@@ -100,7 +113,7 @@ var tokyo = {
     list.appendChild(lastItem);
   }
 };
-
+tokyo.cityName();
 tokyo.generateSales();
 // console.log(tokyo.avgCookieArr);
 tokyo.render();
@@ -108,11 +121,16 @@ tokyo.render();
 
 
 var dubai = {
+  name: 'Dubai',
   minCustRate:11,
   maxCustRate:38,
   avgCookieSale:3.7,
   avgCookieArr:[],
   cookieTotal:0,
+  cityName: function(){
+    var grabName = document.getElementById('store-three-name');
+    grabName.textContent = `${this.name}`;
+  },
   //function to find random customer rate within our min max range
   randomMath: function(){
     return Math.ceil(Math.random() * (this.maxCustRate - this.minCustRate + 1)) + this.minCustRate;
@@ -146,17 +164,23 @@ var dubai = {
     list.appendChild(lastItem);
   }
 };
+dubai.cityName();
 dubai.generateSales();
 // console.log(dubai.avgCookieArr);
 dubai.render();
 // console.log(dubai.cookieTotal);
 
 var paris = {
+  name: 'Paris',
   minCustRate:20,
   maxCustRate:38,
   avgCookieSale:2.3,
   avgCookieArr:[],
   cookieTotal:0,
+  cityName: function(){
+    var grabName = document.getElementById('store-four-name');
+    grabName.textContent = `${this.name}`;
+  },
   //function to find random customer rate within our min max range
   randomMath: function(){
     return Math.ceil(Math.random() * (this.maxCustRate - this.minCustRate + 1)) + this.minCustRate;
@@ -190,17 +214,23 @@ var paris = {
     list.appendChild(lastItem);
   }
 };
+paris.cityName();
 paris.generateSales();
 // console.log(paris.avgCookieArr);
 paris.render();
 // console.log(paris.cookieTotal);
 
 var lima = {
+  name: 'Lima',
   minCustRate:2,
   maxCustRate:16,
   avgCookieSale:4.6,
   avgCookieArr:[],
   cookieTotal:0,
+  cityName: function(){
+    var grabName = document.getElementById('store-five-name');
+    grabName.textContent = `${this.name}`;
+  },
   //function to find random customer rate within our min max range
   randomMath: function(){
     return Math.ceil(Math.random() * (this.maxCustRate - this.minCustRate + 1)) + this.minCustRate;
@@ -234,6 +264,7 @@ var lima = {
     list.appendChild(lastItem);
   }
 };
+lima.cityName();
 lima.generateSales();
 // console.log(lima.avgCookieArr);
 lima.render();
